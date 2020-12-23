adc0031812e58d855b2f064dae3887356c3c7f52
John <john@lucidiansolutions.com>
2020-12-23 17:45:19 +0000

    added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './members/home/home.component';
 import { MemberDashboardComponent } from './members/member-dashboard/member-dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MemberDashboardComponent } from './members/member-dashboard/member-dash
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
