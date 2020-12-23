6a19403695cebf4b31cfb3c87105b52dec4eebef
John <john@lucidiansolutions.com>
2020-12-23 17:43:40 +0000

    local commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {HomeComponent} from "./members/home/home.component";
+import {MemberDashboardComponent} from "./members/member-dashboard/member-dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+    children: []
+  },
+  { path: 'login',
+    component: LoginComponent
+  },
+  { path: 'members',
+    component: MemberDashboardComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './members/home/home.component';
+import { MemberDashboardComponent } from './members/member-dashboard/member-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    MemberDashboardComponent
   ],
   imports: [
     BrowserModule,
